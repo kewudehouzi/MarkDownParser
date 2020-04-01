dbd6b467b18661c2edc73fda5e23ef264abaa2c9
kewudehouzi <40828249+kewudehouzi@users.noreply.github.com>
2020-04-01 23:41:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include "MD_Parser.h"
+#include "MD_Parser.h"//需要实现的markdown解析类
 #include "head.h"
 using namespace std;
 
 
 
 int main(){
-    MarkDownParser transformer("Test.md");
+    MarkDownParser transformer("Test.md");//装载构造markdown文件
 
-    std::string contents = transformer.getContents();
+    std::string contents = transformer.getContents();//获取markdown转成HTML后的内容，即目标代码生成。编译的过程是词法分析+语法分析
 
-    std::string head = Get_Head(), end = Get_End();
+    std::string head = Get_Head(), end = Get_End();//准备写入HTML文件的头尾信息
 
-    std::ofstream out;
+    std::ofstream out;//将结果写入到文件
 
-    out.open("Test2.html");
+    out.open("Test2.html");//
 
     out << head << contents << end << endl;
 }
