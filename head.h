306842fa92ce0146d562cafeead52a66eac31889
kewudehouzi <40828249+kewudehouzi@users.noreply.github.com>
2020-04-02 10:49:52 +0800

    Update head.h

--- head.h
+++ head.h
@@ -2,6 +2,7 @@
 
 
 std::string Get_Head(){
+    //meta提供文档的元数据，link定义文档与外部资源的关系，
     std::string head = "<!DOCTYPE html>\n\t<html>\n\t<head>\
     \n\t\t<meta charset=\"UTF-8\">\n\t\t<title></title>\
     \n\t\t<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/gh/Microsoft/vscode/extensions/markdown-language-features/media/markdown.css\">\n\t\t\
@@ -10,6 +11,7 @@ std::string Get_Head(){
 
     return head;
 }
+
 
 std::string Get_End(){
     std:: string end= "</body></html>";
